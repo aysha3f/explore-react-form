524334ec2b3927d1ef652ac4066c2c582b7477f8
aysha3f <tahiaysha220@gmail.com>
2025-12-08 19:48:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Simpleform from './components/Simpleform/Simpleform'
+
+function App() {
+
+
+  return (
+    <>
+     
+     
+      <h1>Explore React Form</h1>
+      <Simpleform></Simpleform>
+    </>
+  )
+}
+
+export default App
