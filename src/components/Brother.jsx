31ea387297104b4ecf19b71622f8aa6860019dd4
aysha3f <tahiaysha220@gmail.com>
2025-12-23 21:42:57 +0600

    Explore more context API and module summary

--- src/components/Brother.jsx
+++ src/components/Brother.jsx
@@ -1,10 +1,15 @@
 
-import React from 'react';
+import React, { use } from 'react';
+import { MoneyContext } from './FamilyTree/FamilyTree';
 
 const Brother = () => {
+ 
+ const [money, setMoney] = use(MoneyContext);
+ 
     return (
         <div>
             <h3>Brother</h3>
+            <button onClick={()=> setMoney(money + 1000)}>Add 1000 tk</button>
         </div>
     );
 };
--- src/components/FamilyTree/Aunt.jsx
+++ src/components/FamilyTree/Aunt.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { use } from 'react';
 import Cousins from './Cousins';
+import { MoneyContext } from './FamilyTree';
 
 const Aunt = ({asset}) => {
+   
+   const [money, setMoney] = use(MoneyContext);
+   const handleAddMoney = () =>{
+    setMoney(money + 5000);
+   }
     return (
         <div>
             <h3>Aunt</h3>
             <section className='flex'>
              <Cousins asset={asset} name='Tom Tom'></Cousins>  
               <Cousins name='Jo jooo'></Cousins>
+              <button onClick={handleAddMoney}>Add 5000 tk</button>
             </section>
         </div>
     );
